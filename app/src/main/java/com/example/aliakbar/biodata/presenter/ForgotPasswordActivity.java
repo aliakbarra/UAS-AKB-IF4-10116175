23bde7192b4431601c655b050ff6a94b187df8cf
user <ali.thegame40@gmail.com>
2019-08-13 00:52:52 +0700

    hello

---
+++ app/src/main/java/com/example/aliakbar/biodata/presenter/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.aliakbar.biodata.presenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.aliakbar.biodata.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/example/aliakbar/biodata/presenter/ILoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.aliakbar.biodata.presenter;
+
+public interface ILoginActivity {
+
+    void chekInputan();
+    void masuk();
+    boolean cekPassword(String Password);
+    boolean cekUser(String Username);
+
+}
